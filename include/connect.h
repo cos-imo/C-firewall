4a6ac93a19e6911b1180defb480fde149a2fc252
Cosimo Ungaro <cosimo.ungaro@telecomnancy.net>
2024-10-05 10:55:24 +0200

    added activation

--- include/connect.h
+++ include/connect.h
@@ -3,8 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pcap/pcap.h>
 
-int messages();
+#define SIGN(z) ((z>0) - (z<0))
 
 #endif /* __CONNECT_H__ */
--- src/connect.c
+++ src/connect.c
@@ -5,13 +5,37 @@ int main(int argc, char* *argv){
 	char errbuf[PCAP_ERRBUF_SIZE];
 	memset(errbuf, 0, PCAP_ERRBUF_SIZE);
 	pcap_if_t *alldevsp = NULL;
+	pcap_t *handle = NULL;
+	int activate_status, activate_sign;
 
 	if (pcap_findalldevs(&alldevsp, errbuf) == PCAP_ERROR) {
 		printf("failed to scan for devices: %s \n", errbuf);
 		exit(PCAP_ERROR);
 	}
 
+	handle = pcap_create(alldevsp->name, errbuf);
+
+	if (handle == NULL) {
+		printf("Couldn't create handle: %s \n", errbuf);
+		exit(1);
+	}
+
+	activate_status = pcap_activate(handle);
+
+	switch(SIGN(activate_status)){
+		case 1:
+			printf("error activating capture: %s \n", pcap_statustostr(activate_status));
+			break;
+		case -1:
+			printf("error activating capture: %s \n", pcap_statustostr(activate_status));
+			break;
+		default:
+			printf("ok");
+			break;
+	}
+
 	pcap_freealldevs(alldevsp);
 
 	return 0;
+
 }
