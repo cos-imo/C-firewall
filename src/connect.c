8f1ed7c91b61ccc51d64ce33320cdd9c744e832d
Cosimo Ungaro <cosimo.ungaro@telecomnancy.net>
2024-10-05 10:18:38 +0200

    getting interface

--- src/connect.c
+++ src/connect.c
@@ -1,27 +1,17 @@
 #include "connect.h"
 
-int main(int argc, char* *argv) {
+int main(int argc, char* *argv){
 
-	int msg = messages();
-
-	return msg;
-
-}
-
-int messages(){
 	char errbuf[PCAP_ERRBUF_SIZE];
+	memset(errbuf, 0, PCAP_ERRBUF_SIZE);
+	pcap_if_t *alldevsp = NULL;
 
-	pcap_if_t* *alldevsp;
-	int dev;
-
-	dev = pcap_findalldevs(alldevsp, errbuf);
-	if (dev == PCAP_ERROR) {
-		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-		return(2);
+	if (pcap_findalldevs(&alldevsp, errbuf) == PCAP_ERROR) {
+		printf("failed to scan for devices: %s \n", errbuf);
+		exit(PCAP_ERROR);
 	}
 
+	pcap_freealldevs(alldevsp);
 
 	return 0;
 }
-
-
